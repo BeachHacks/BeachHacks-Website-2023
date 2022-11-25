74295d260f7349438f4a82585a5857b643f7c4d2
Diego O Garcia <diegogar577@gmail.com>
2022-11-24 23:10:40 -0800

    more grid changes
    getting there hmmm

--- src/views/Sponsors/Sponsors.js
+++ src/views/Sponsors/Sponsors.js
@@ -1,10 +1,12 @@
 // create react component
 import React from "react";
 import { Container, Grid, styled } from "@mui/material";
-import img from "../../components/Images/Rectangle_122.png";
-import img2 from "../../components/Images/Rectangle_479x540.png";
+import img from "../../components/Images/Rectangle_257x257.png";
+import img2 from "../../components/Images/Rectangle_168x167.png";
+import img3 from "../../components/Images/Rectangle_479x540.png";
+
 const Img = styled("img")({
-  margin: "auto",
+  margin: "5% 0",
   display: "block",
   maxWidth: "100%",
   maxHeight: "100%",
@@ -26,24 +28,69 @@ const Sponsors = () => {
           </h1>
         </Grid>
       </Grid>
-      <Grid
-        container
-        spacing={2}
-        direction={"row"}
-        className={"sponsor-row"}
-        justifyContent="flex-start"
-        flexGrow={1}
-        style={{}}
-      >
-        <Grid item xs={2}>
-          <Img alt="place-holder-1" src={img} />
+
+      <Grid container className={"sponsor-container"}>
+
+        <Grid item xs={6} className={"left-nine-sponsors"} justifyContent={"center"}>
+          <Grid container gap={"53px"} wrap={"nowrap"}>
+            <Grid item xs={4}>
+              <Img alt="place-holder-1" src={img} />
+            </Grid>
+            <Grid item xs={4}>
+              <Img alt="place-holder-2" src={img} />
+            </Grid>
+            <Grid item xs={4}>
+              <Img alt="place-holder-3" src={img} />
+            </Grid>
+          </Grid>
+          <Grid container columnGap={"37px"} wrap={"nowrap"}>
+            <Grid item xs={2}>
+              <Img alt="place-holder-4" src={img2} />
+            </Grid>
+            <Grid item xs={2}>
+              <Img alt="place-holder-5" src={img2} />
+            </Grid>
+            <Grid item xs={2}>
+              <Img alt="place-holder-6" src={img2} />
+            </Grid>
+            <Grid item xs={2}>
+              <Img alt="place-holder-7" src={img2} />
+            </Grid>
+            <Grid item xs={2}>
+              <Img alt="place-holder-8" src={img2} />
+            </Grid>
+            <Grid item xs={2}>
+              <Img alt="place-holder-9" src={img2} />
+            </Grid>
+          </Grid>
+
+          <Grid container columnGap={"37px"} wrap={"nowrap"} zeroMinWidth>
+            <Grid item xs={2}>
+              <Img alt="place-holder-10" src={img2} />
+            </Grid>
+            <Grid item xs={2}>
+              <Img alt="place-holder-11" src={img2} />
+            </Grid>
+            <Grid item xs={2}>
+              <Img alt="place-holder-12" src={img2} />
+            </Grid>
+            <Grid item xs={2}>
+              <Img alt="place-holder-13" src={img2} />
+            </Grid>
+            <Grid item xs={2}>
+              <Img alt="place-holder-14" src={img2} />
+            </Grid>
+            <Grid item xs={2}>
+              <Img alt="place-holder-15" src={img2} />
+            </Grid>
+          </Grid>
         </Grid>
-        <Grid item xs={2}>
-          <Img alt="place-holder-2" src={img} />
+        <Grid item xs={6} >
+          <Grid container className={"right-sponsor"} justifyContent={"center"} marginTop={"0%"}>
+            <Img alt="place-holder-16" src={img3} />
+          </Grid>
         </Grid>
-        <Grid item xs={2}>
-          <Img alt="place-holder-3" src={img} />
-        </Grid>
+
       </Grid>
     </Container>
   );
