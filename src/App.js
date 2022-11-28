3dc91cf1f48d1df611f8d03c26415a8df5ac452e
porterclev <porterclev@gmail.com>
2022-11-27 22:34:15 -0800

    Dummy code for App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
+import About from "./views/FAQ/About"
 
 function App() {
-  return <h1>Home Page</h1>;
+  // return <h1>Home Page</h1>;
+  return <div>
+    <About />
+  </div>
 }
 
 export default App;
