f7e6bfc627e20c22f55749e79b9cb04fbd8baee9
Porter Clevidence <porterclev@gmail.com>
2022-11-28 16:31:18 -0800

    Inserted Dummy Qs & As

--- src/views/FAQ/About.js
+++ src/views/FAQ/About.js
@@ -6,28 +6,28 @@ const About = () => {
   const [questions, setQuestions] = useState([
     {
       Q: 'Question',
-      P: 'Stuff',
+      P: 'Answers',
       id: 1,
       expand: false,
     }, 
 
     {
       Q: 'Question',
-      P:'',
+      P:'Answers',
       id: 2,
       expand: false,
     },
 
     {
       Q: 'Question',
-      P: '',
+      P: 'Answers',
       id: 3,
       expand: false,
     },
 
     {
       Q: 'Question',
-      P: '',
+      P: 'Answers',
       id: 4,
       expand: false,
     }
@@ -40,16 +40,16 @@ const About = () => {
     <section className="faq-container" id="faq">
       {/* FAQ's */}
       <div className="header">
-          <h1>FAQ's</h1>
+        <h1>FAQ's</h1>
       </div>
 
       {/* Dropdown bars */}
       <div className="question">
-      {questions.length > 0 ? (
-        <Questions questions={questions} onToggle={toggleQuestion}/>
-      ) : (
-        'None'
-      )}
+        {questions.length > 0 ? (
+          <Questions questions={questions} onToggle={toggleQuestion}/>
+        ) : (
+          'None'
+        )}
       </div>
       
       
