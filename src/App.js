3cd3c121f4da278ff4cc1b9c66f065c5c10fb298
Rodolfo Sandoval <rodolfo.rudy.sandoval@gmail.com>
2022-12-02 04:31:45 -0800

    initial layout, font fixes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import About from "./views/About/About";
+import Apply from "./views/Apply/Apply";
 
 function App() {
   // return <h1>Home Page</h1>;
   return (
     <div className="App">
       <About/>
+      <Apply/>
     </div>
   )
 }
