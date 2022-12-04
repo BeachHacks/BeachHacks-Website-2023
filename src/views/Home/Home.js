de52d176d8394f014811caa2fc1f47a6147c5fc4
LeeNguyen <116839213+LeeNguyen29@users.noreply.github.com>
2022-12-03 20:01:53 -0800

    update home page

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -3,20 +3,19 @@ import bh_logo from "../../assets/bh_logo.svg";
 
 const Home = () => {
     return (
-        <section className="home-container">
-            <div className="home-page" id="home">
-                <div className="home-app-section">
-                    <div className="home-app-title">
-                        <img src={bh_logo} alt="BeachHacks" className="home-app-logo" />   
-                    </div>
-                        <h1>Date, 2023</h1>
-                        <h1>Get Involved</h1>
-                        <button>Apply Now</button>
+        <section className="home-container" id="home">
+            <div className="home-app-section">
+                <div className="home-app-title">
+                    <img src={bh_logo} alt="BeachHacks" className="home-app-logo" />   
                 </div>
-                <div className="home-box-section">
-                    <div className="home-box">
-                        <h1 className="home-box-title">3D ISOMETRIC ART?</h1>
-                    </div>
+                <h3>Date, 2023</h3>
+                <h3>Get Involved</h3>
+                <button>Apply Now</button>
+            </div>
+            
+            <div className="home-box-section">
+                <div className="home-box">
+                    <h1 className="home-box-title">3D ISOMETRIC ART?</h1>
                 </div>
             </div>
         </section>
