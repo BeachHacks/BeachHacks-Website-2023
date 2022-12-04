65fcc4bcbcfe77cc1aab445b7ebfa4a993596126
LeeNguyen <116839213+LeeNguyen29@users.noreply.github.com>
2022-12-03 16:56:44 -0800

    initial-layout

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,11 +1,25 @@
 import "./Home.css";
+import bh_logo from "../../assets/bh_logo.svg";
 
 const Home = () => {
     return (
-        <div className="home-page" id="home">
-            {/* build the home section here */}
-            <h1>Home</h1>
-        </div>
+        <section className="home-container" id="home"> 
+            <div className="home-page" id="home">
+                <div className="home-app-section">
+                    <div className="home-app-title">
+                        <img src={bh_logo} alt="BeachHacks" className="home-app-logo" />
+                        <h1>BeachHacks</h1>
+                        <h3>BeachHacks starting soon</h3> 
+                    </div>    
+                </div>           
+            </div>
+            <div className="home-date-section">
+                <h3 className="home-box-title">Hackathon - Date and Location</h3>
+                <button>JOIN NOW</button>
+                <button>click</button>
+
+            </div>
+        </section>
     );
 }
 export default Home;
