1c4b0899f67ff6c551d9e0ba98074d5208a8b671
Lydia Yang <lydiayang92@gmail.com>
2022-12-26 22:39:52 -0800

    added curly brace

--- src/App.js
+++ src/App.js
@@ -3,22 +3,25 @@ import React from "react";
 import Header from "./components/Header/Header";
 import Home from "./views/Home/Home";
 import Team from "./views/Team/Team";
-import {Route, Routes} from "react-router-dom";
-import Footer from "./components/Footer/Footer"
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-       <Routes>
-        <Route path="/Home" element={<Home/>} />
-        <Route path="/Team" element={<Team/>} />
-        <Route path="/Join" component={() => {
-    window.location.href = 'https://www.google.com/';
-    return null;
-    }}/>
+      <Header />
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Team" element={<Team />} />
+        <Route
+          path="/Join"
+          component={() => {
+            window.location.href = "https://www.google.com/";
+            return null;
+          }}
+        />
       </Routes>
     </div>
   );
-  
+}
 export default App;
