9ad10f4bf69026fe4197957c477d03590ba5c0e0
Lydia Yang <lydiayang92@gmail.com>
2022-12-30 15:33:09 -0800

    made some changes

--- src/App.js
+++ src/App.js
@@ -3,25 +3,29 @@ import React from "react";
 import Header from "./components/Header/Header";
 import Home from "./views/Home/Home";
 import Team from "./views/Team/Team";
-import About from "./views/About/About"
-import FAQ from "./views/FAQ/FAQ"
-import {Route, Routes} from "react-router-dom";
-
+import About from "./views/About/About";
+import FAQ from "./views/FAQ/FAQ";
+import SideNavBar from "./components/SideNavbar/SideNavbar";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-       <Routes>
-        <Route path="/Home" element={<Home/>} />
-        <Route path="/Team" element={<Team/>} />
-        <Route path="/About" element={<About/>} />
-        <Route path="/FAQ" element={<FAQ/>} />
-        <Route path="/Join" component={() => {
-    window.location.href = 'https://www.google.com/';
-    return null;
-    }}/>
-      </Routes>
+      <SideNavBar />
+      {/* <Header />
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Team" element={<Team />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/FAQ" element={<FAQ />} />
+        <Route
+          path="/Join"
+          component={() => {
+            window.location.href = "https://www.google.com/";
+            return null;
+          }}
+        />
+      </Routes> */}
     </div>
   );
 }
