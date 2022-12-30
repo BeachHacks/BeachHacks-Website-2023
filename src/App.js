6b23009dfc82721ff103f7706aea4cbbbb9f4328
Brian Poon <86692163+Brian-S-Poon@users.noreply.github.com>
2022-12-30 13:19:28 -0800

    Added edits in app.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import "./App.css";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+  } from "react-router-dom";
+   
+  import './App.css';
+  import Sidenav from './Components/SideNavbar';
+  import Home from "./Pages/Home";
+  import About from "./Pages/About";
+  import Sponsors from "./Pages/Sponsors";
+  import FAQ from "./Pages/FAQ";
 
-function App() {
-  return <h1>Home Page</h1>;
-}
-
-export default App;
+   
+  function App() {
+    return (
+      <div className="App">
+        <Sidenav/>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/about" element={<About />} />
+            <Route path="/sponsors" element={<Sponsors />}/>
+            <Route path="/faq" element={<FAQ />} />
+          </Routes>
+        </main>
+      </div>
+    );
+  }
+  export default App;
