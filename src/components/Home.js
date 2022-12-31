1c9f0499cf55a2521219e90760ea051c63a7d81a
LeeNguyen <116839213+LeeNguyen29@users.noreply.github.com>
2022-12-30 19:15:38 -0800

    update responsiveness
    changed to percentage instead of using rem. it's for scaling purposes

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import "./Home.css";
+import bh_logo from "../../assets/bh_logo.svg";
+
+const Home = () => {
+    return (
+        <section className="home-container" id="home">
+            <div className="home-app-section">
+                <div className="home-app-title">
+                    <img src={bh_logo} alt="BeachHacks" className="home-app-logo" />   
+                </div>
+                <h2>Date, 2023</h2>
+                <h3>Get Involved</h3>
+                <button>Apply Now</button>
+            </div>
+            
+            <div className="home-box-section">
+                <div className="home-box">
+                    <h2>3D ISOMETRIC ART?</h2>
+                </div>
+            </div>
+        </section>
+   );
+}
+
+export default Home;
