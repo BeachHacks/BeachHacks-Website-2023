52502134aaac79dacd0bb67dce4686f6a674caf7
porterclev <porterclev@gmail.com>
2023-01-17 18:46:20 -0800

    added questions and fixed chevron

--- src/views/FAQ/FAQ.js
+++ src/views/FAQ/FAQ.js
@@ -5,11 +5,12 @@ import { useState } from "react";
 const FAQ = () => {
   const [questions, setQuestions] = useState([
     {
-      Q: "Question",
+      Q: "What is a Hackathon?",
       P: (
         <>
-          Answers
-          <br />
+          A hackathon is an event where individuals or teams come together to
+          collaborate and build innovative projects in a short period of time,
+          usually within 24 hours.
         </>
       ),
       id: 1,
@@ -17,23 +18,26 @@ const FAQ = () => {
     },
 
     {
-      Q: "Question",
-      P: (
-        <>
-          Answers
-          <br />
-        </>
-      ),
+      Q: "Is BeachHacks 7.0 online or in-person?",
+      P: <>BeachHacks is back to in-person only!</>,
       id: 2,
       expand: false,
     },
 
     {
-      Q: "Question",
+      Q: "Who can participate in BeachHacks?",
       P: (
         <>
-          Answers
+          BeachHacks registration is open to all college students, regardless of
+          major! We also accept registrations from CSULB alumni who have
+          graduated within the past year. Click [here] to submit your
+          registration!
           <br />
+          Due to the limited space, we have a cap of 150 participants. Once
+          we've received all the registrations, we'll send out emails to let you
+          know if you've been selected to participate. We'll do our best to
+          accommodate as many participants as possible, but please keep in mind
+          that current CSULB students will receive priority.
         </>
       ),
       id: 3,
@@ -41,14 +45,80 @@ const FAQ = () => {
     },
 
     {
-      Q: "Question",
+      Q: "How do I register for BeachHacks?",
+      P: (
+        <>
+          We're thrilled that you're interested in joining us for BeachHacks! To
+          register, simply fill out the registration form on our website.
+          <br />
+          Due to the limited space, we have a cap of 150 participants. Once
+          we've received all the registrations, we'll send out emails to let you
+          know if you've been selected to participate. We'll do our best to
+          accommodate as many participants as possible, but please keep in mind
+          that current CSULB students will receive priority.
+        </>
+      ),
+      id: 4,
+      expand: false,
+    },
+
+    {
+      Q: "What type of projects can I work on at BeachHacks?",
+      P: (
+        <>
+          Project types can vary widely and can be in the areas such as web
+          development, software development, hardware, game development, data
+          science, artificial intelligence, virtual reality, internet of things,
+          and many more.
+          <br />
+          However, it's important to note that all projects must be started
+          during the event and should not be pre-existing projects. This is to
+          ensure the spirit of creativity, innovation, and teamwork is upheld
+          throughout the hackathon. We encourage you to be creative and think
+          outside the box when coming up with ideas!
+        </>
+      ),
+      id: 5,
+      expand: false,
+    },
+
+    {
+      Q: "What are the rules for participating in BeachHacks?",
       P: (
         <>
           Answers
           <br />
         </>
       ),
-      id: 4,
+      id: 6,
+      expand: false,
+    },
+
+    {
+      Q: "What should I bring to BeachHacks?",
+      P: (
+        <>
+          You should bring your own computer, chargers, necessary equipment, and
+          any other materials you may need to work on your project. For hardware
+          projects, we will have a selection of materials from our sponsor,
+          Arduino!
+          <br />
+        </>
+      ),
+      id: 7,
+      expand: false,
+    },
+
+    {
+      Q: "How can I contact the BeachHacks team if I have additional questions?",
+      P: (
+        <>
+          Contact our team through our social media channels, or email us at
+          beachhacks@csulb.acm.org. We look forward to hearing from you!
+          <br />
+        </>
+      ),
+      id: 8,
       expand: false,
     },
   ]);
