2a91e6577e67bd3f6f5fe8c7d4bd600a8e8dda12
Lydia Yang <lydiayang92@gmail.com>
2023-01-29 20:52:22 -0800

    changed spotify to beachmuse and the icon on the header

--- src/pages/Spotify/Spotify.js
+++ src/pages/Spotify/Spotify.js
@@ -1,12 +1,12 @@
-import "./Spotify.css"
+import "./Spotify.css";
 
 const Spotify = () => {
-    return (
-        <div className="spotify-container">
-            <h1>Spotify Queue</h1>
-            <h2>Coming Soon...</h2>
-        </div>
-    )
-}
+  return (
+    <div className="spotify-container">
+      <h1>BeachMuse</h1>
+      <h2>Coming Soon...</h2>
+    </div>
+  );
+};
 
-export default Spotify
+export default Spotify;
